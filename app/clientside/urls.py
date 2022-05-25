9c3634880c5d1a4cc0281954d3bdbe51f3ed4694
Dionisiu Brovka <dev.dionisiu.brovka@gmail.com>
2022-05-25 10:52:43 +0300

    remake map

--- app/clientside/urls.py
+++ app/clientside/urls.py
@@ -3,4 +3,8 @@ from .views import *
 
 urlpatterns = [
     path('', pg_index),
+    path('add_form', pg_index),
+    path('edit_form', pg_index),
+    path('auth', pg_index),
+    path('api/points', pg_api_points),
 ]
--- app/clientside/views.py
+++ app/clientside/views.py
@@ -1,9 +1,51 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+from django.template.loader import render_to_string
+from .models import Lavochki
 
 # Create your views here.
  
 def pg_index(request):
-    return render(request, "pages/index.html")
+    data = {
+        'objs' : Lavochki.objects.filter(is_valid=True)
+    }
+    return render(request, "pages/index.html", data)
 
-def pg_form(request):
-    return render(request, "pages/form.html")
+def pg_add_form(request):
+    return render(request, "pages/add_form.html")
+
+def pg_edit_form(request):
+    return render(request, "pages/edit_form.html")
+
+def pg_auth(request):
+    return render(request, "pages/auth.html")
+
+def pg_api_points(request):
+    data = {
+        "type": "FeatureCollection",
+        "features": []
+    }
+
+    lavochki_obj_list = Lavochki.objects.filter(is_valid=True)
+    
+    for lavochka in lavochki_obj_list:
+        
+        lavochka = {
+            "type": "Feature",
+            "id": lavochka.id, 
+            "geometry": {
+                "type": "Point", 
+                "coordinates": [lavochka.x, lavochka.y]
+            }, 
+            "properties": {
+                "balloonContentHeader": render_to_string('el/map_balloon/head.html', {'id':lavochka.id}), 
+                "balloonContentBody": "<p>Ваше имя: <input name='login'></p><p><em>Телефон в формате 2xxx-xxx:</em>  <input></p><p><input type='submit' value='Отправить'></p>", 
+                "balloonContentFooter": "<font size=1>Информация предоставлена: </font> <strong>этим балуном</strong>", 
+                "clusterCaption": "<strong><s>Еще</s> одна</strong> метка", 
+                "hintContent": "<strong>Текст  <s>подсказки</s></strong>"
+            }
+        }
+
+        data["features"].append(lavochka)
+
+    return JsonResponse(data)
