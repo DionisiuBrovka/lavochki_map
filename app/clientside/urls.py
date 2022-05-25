29af26777af6ae4e33767b6a51553811fe7ad0c8
animeonelovee <65125641+animeonelovee@users.noreply.github.com>
2022-05-25 17:37:47 +0300

    add authorization

--- app/clientside/urls.py
+++ app/clientside/urls.py
@@ -5,6 +5,7 @@ urlpatterns = [
     path('', pg_index),
     path('add_form', pg_add_form),
     path('edit_form', pg_index),
-    path('auth', pg_index),
+    path('auth', pg_auth),
     path('api/points', pg_api_points),
+    path('accounts/', include('django.contrib.auth.urls')),
 ]
