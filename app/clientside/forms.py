cc0df7e3cc77ab2a6562543f2858fdbfa5f7dfe9
Dionisiu Brovka <dev.dionisiu.brovka@gmail.com>
2022-05-25 12:11:36 +0300

    add form chng #2

--- app/clientside/forms.py
+++ app/clientside/forms.py
@@ -1,7 +1,7 @@
 from django import forms
 
 class AddLavochkaForm(forms.Form):
-    x = forms.FloatField()
-    y = forms.FloatField()
+    x = forms.FloatField(widget=forms.NumberInput(attrs={'class': "form-control"}))
+    y = forms.FloatField(widget=forms.NumberInput(attrs={'class': "form-control"}))
     disc = forms.CharField(widget=forms.Textarea(attrs={'class': "form-control"}))
     photo = forms.ImageField()
